a35233172a3ea1266b928d96ca725b2c417a14c6
pastillazz <robertoalonsopelayo26@gmail.com>
2026-02-17 03:57:30 -0500

    holamundo 3

---
+++ 01hello.py
@@ -0,0 +1,9 @@
+my_variable = 42
+my_string_variable="holaa"
+print(my_variable)
+
+print(my_variable,my_string_variable,type(my_string_variable), type(my_variable), type(str(my_variable)))
+
+my_int_to_float =float(42)
+
+print(type(my_int_to_float))
